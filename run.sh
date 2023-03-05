5f2b6b2ec586f0f67a42a2aeb68da6e27bc28b2d
Maximilian <108086088+ONEMILIAN@users.noreply.github.com>
2023-03-05 19:19:34 +0100

    1.0.6

---
+++ run.sh
@@ -0,0 +1,11 @@
+echo "Starting..."
+echo "\n"
+echo "Type 1 or 0..."
+echo "\n"
+echo "1 = GUI-Filebrowser"
+echo "0 = Terminal-Filebrowser"
+echo "\n"
+read arg
+python3 main.py $arg
+echo "\n"
+echo "Finished!\nThank you for using OM-Filebrowser!"
